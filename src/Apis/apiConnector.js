be6e76115fb40efde6e6bf9e8b6f8c1517ec3395
KarishmaGR <karishmasvryadav@gmail.com>
2024-04-15 00:34:43 +0530

    first commit

--- src/Apis/apiConnector.js
+++ src/Apis/apiConnector.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+axios.defaults.headers.common["Origin"] =
+  "https://job-board-frontend-teal.vercel.app";
+
 axios.defaults.withCredentials = true;
+
 const axiosInstance = axios.create({});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
